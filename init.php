63397f5492edf58f1be246107a1058373dfc7966
Tikhon Sokolov <tikhon.sokolov>
2025-02-11 19:25:53 +0300

    require functions

--- init.php
+++ init.php
@@ -1,4 +1,8 @@
 <?php
 
+require_once __DIR__ . '/functions/check_email.php';
+require_once __DIR__ . '/functions/send_email.php';
+
 $dbPath = __DIR__ . '/db/db.sqlite';
 $db = new SQLite3($dbPath);
+$db->enableExceptions(true);
